7647abfb658c25013e6c1fd6893063e00ffec4a8
harony1122 <45592826+harony1122@users.noreply.github.com>
2018-12-23 19:47:11 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ client.on('ready', () => {
 });
 
 
-const developers = ["295210953384132618"]
+const developers = ["472528809401253889"]
 const adminprefix = "^";
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
